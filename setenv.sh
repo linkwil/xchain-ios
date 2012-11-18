a9b368ed95806ee9c7cec07dc27e5c8ecb1a349a
polyvertex <polyvertex@gmail.com>
2012-11-18 10:15:44 +0100

    Added CODESIGN_ALLOCATE variable to environment.

--- setenv.sh
+++ setenv.sh
@@ -4,11 +4,14 @@ export TARGET=arm-apple-darwin
 export PREFIX=/usr/$TARGET
 export DARWIN_PREFIX=$PREFIX
 
-echo "Target: $TARGET"
-echo "Prefix: $PREFIX"
+export PATH="$PATH:/usr/$TARGET/usr/bin"
+
+[ -z "$CODESIGN_ALLOCATE" ] && export CODESIGN_ALLOCATE="${TARGET}-codesign_allocate"
 
 [ -e "$PREFIX" ] \
   || mkdir -p "$PREFIX" \
   || echo "** ERROR: Failed to create \"$PREFIX\" !"
 
-export PATH="$PATH:/usr/$TARGET/usr/bin"
+echo "Target: $TARGET"
+echo "Prefix: $PREFIX"
+echo "CODESIGN_ALLOCATE: $CODESIGN_ALLOCATE"
